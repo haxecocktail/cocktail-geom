248035d6436d3bf38da38df40992a4bb68a82f08
yannick <yannick.dominguez@gmail.com>
2014-07-01 20:37:13 +0200

    switch to buddy, start to write specs

---
+++ test/unit/TestMain.hx
@@ -0,0 +1,13 @@
+package unit;
+
+import buddy.*;
+using buddy.Should;
+
+import cocktail.geom.*;
+
+class TestMain extends BuddySuite implements Buddy {
+
+    public function new()
+    {
+    }
+}
---
+++ test/unit/TestMatrix.hx
@@ -0,0 +1,22 @@
+package unit;
+
+import buddy.*;
+using buddy.Should;
+
+import cocktail.geom.*;
+
+class TestMatrix extends BuddySuite implements Buddy {
+
+    public function new()
+    {
+        describe('Matrix', function () {
+            describe('#identity', function (){
+                it('should reset the matrix', function () {
+
+                });
+
+            });
+
+        });
+    }
+}
